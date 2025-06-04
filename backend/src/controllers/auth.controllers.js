e25f4003d8598e468dd6a13e4d7fb834a61b8623
Faraz Ahmad <farazahmad@Farazs-MacBook-Air.local>
2025-06-04 14:20:00 +0530

    project completed

---
+++ backend/src/controllers/auth.controllers.js
@@ -0,0 +1,122 @@
+import { generateToken } from "../lib/utils.lib.js";
+ 
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs";
+
+import cloudinary from "../lib/cloudinary.lib.js";
+
+
+
+export const signup = async (req, res) => {
+    const { fullName, email, password } = req.body;
+
+    try {
+        if (!fullName || !email || !password) {
+            return res.status(400).json({ message: "All fields required" });
+        }
+
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password must be at least 6 characters long" });
+        }
+
+        const user = await User.findOne({ email });
+        if (user) return res.status(400).json({ message: "Email already exists" });
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await User.create({
+            email: email,
+            fullName: fullName,
+            password: hashedPassword
+        });
+
+        if (newUser) {
+            try {
+                generateToken(newUser._id, res);
+            } catch (tokenError) {
+                console.log("Token error:", tokenError.message);
+                return res.status(500).json({ message: "Token generation failed" });
+            }
+
+            res.status(201).json({
+                _id: newUser._id,
+                email: newUser.email,
+                fullName: newUser.fullName,
+                profilePic: newUser.profilePic,
+            });
+        } else {
+            res.status(400).json({ message: "Invalid user data" });
+        }
+    } catch (error) {
+        console.log("Signup error stack:", error.stack);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(400).json({ message: "Invalid Credentials" });
+        }
+
+            const isPasswordCorrect = await bcrypt.compare(password, user.password);  
+            if (!isPasswordCorrect) {
+                return res.status(400).json({ message: "Invalid Credentials" });
+            }
+
+            generateToken(user._id, res);
+            res.status(200).json({
+                _id: user._id,
+                email: user.email,
+                fullName: user.fullName,
+                profilePic: user.profilePic,
+            });
+        }
+        catch (error) {
+        console.log("Login error controller:", error.stack);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const logout = (req, res) => {
+    try {
+        res.cookie("jwt","",{maxAge: 0});
+        res.status(200).json({ message: "User logged out successfully" });
+    } catch (error) {
+        console.log("Login error controller:", error.stack);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const updateProfile = async (req, res) => {
+    try {
+        const {profilePic} = req.body;
+        const userId = req.user._id;
+        
+        if(!profilePic) {
+            return res.status(400).json({ message: "Profile picture is required" });
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+
+        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true});
+
+        res.status(400).json({updatedUser});
+    } catch (error) {
+        console.log("Update profile error controller:", error.stack);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("Check auth error controller:", error.stack);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
